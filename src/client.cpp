02f9c9f3aa503766bb42225a94feed3cdf5bf7fa
mideter <mideter@yandex.ru>
2026-04-25 09:46:18 +0300

    Refactor client implementation to utilize SocketHandle for socket management

--- src/client.cpp
+++ src/client.cpp
@@ -1,12 +1,14 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <unistd.h>
 
 #include <cerrno>
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <system_error>
+
+#include "sockethandle.h"
 
 
 namespace {
@@ -25,47 +27,43 @@ void print_errno(const std::string& message) {
 
 
 int main() {
-	const int sock = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0) {
-		print_errno("socket failed");
+	try {
+		SocketHandle sock(::socket(AF_INET, SOCK_STREAM, 0));
+
+		sockaddr_in server_addr{};
+		server_addr.sin_family = AF_INET;
+		server_addr.sin_port = htons(kPort);
+
+		if (::inet_pton(AF_INET, kServerIp, &server_addr.sin_addr) <= 0) {
+			print_errno("inet_pton failed");
+			return 1;
+		}
+
+		if (::connect(sock.get(), reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
+			print_errno("connect failed");
+			return 1;
+		}
+
+		const std::string message = "Hello, I want to get it back!";
+		if (::send(sock.get(), message.c_str(), message.size(), 0) < 0) {
+			print_errno("send failed");
+			return 1;
+		}
+
+		char buffer[1024] = {0};
+		const ssize_t bytes_received = ::recv(sock.get(), buffer, sizeof(buffer) - 1, 0);
+		if (bytes_received < 0) {
+			print_errno("recv failed");
+			return 1;
+		}
+
+		buffer[bytes_received] = '\0';
+		std::cout << "Server replied: " << buffer << '\n';
+
+		return 0;
+	} catch (const std::system_error& e) {
+		std::cerr << e.what() << '\n';
 		return 1;
 	}
-
-	sockaddr_in server_addr{};
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(kPort);
-
-	if (::inet_pton(AF_INET, kServerIp, &server_addr.sin_addr) <= 0) {
-		print_errno("inet_pton failed");
-		::close(sock);
-		return 1;
-	}
-
-	if (::connect(sock, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr)) < 0) {
-		print_errno("connect failed");
-		::close(sock);
-		return 1;
-	}
-
-	const std::string message = "Hello, I want to get it back!";
-	if (::send(sock, message.c_str(), message.size(), 0) < 0) {
-		print_errno("send failed");
-		::close(sock);
-		return 1;
-	}
-
-	char buffer[1024] = {0};
-	const ssize_t bytes_received = ::recv(sock, buffer, sizeof(buffer) - 1, 0);
-	if (bytes_received < 0) {
-		print_errno("recv failed");
-		::close(sock);
-		return 1;
-	}
-
-	buffer[bytes_received] = '\0';
-	std::cout << "Server replied: " << buffer << '\n';
-
-	::close(sock);
-	return 0;
 }
 
